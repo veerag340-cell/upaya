ae05bbb13b7905b009cff9551243c143512721c8
Venu <veerag340@gmail.com>
2026-02-20 20:59:41 +0530

    Removed theBlog in page.tsx

--- package/src/app/page.tsx
+++ package/src/app/page.tsx
@@ -9,7 +9,7 @@ import ServicesPreview from '@/components/Home/ServicesPreview';
 import ProfessionalProfile from '@/components/Home/ProfessionalProfile';
 import Portfolio from '@/components/SharedComponent/portfollio'
 import Testimonial from '@/components/SharedComponent/Testimonial' 
-import Blog from '@/components/SharedComponent/Blog'
+// import Blog from '@/components/SharedComponent/Blog'
 import Contactform from '@/components/Home/Contact';
 export const metadata: Metadata = {
   title: "Upaya",
